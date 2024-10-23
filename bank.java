6dda7b4b8b4f44c441283a9152dd34cde323a354
RAKSHA B R <rakshagowdah264@gmail.com>
2024-10-23 23:20:54 +0530

    Added code

--- bank.java
+++ bank.java
@@ -1,107 +1,131 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*2 . Define a class to represent a bank ACCOUNT and include the following members:
+    I. Data Members(States):
+      a) Name of Depositor
+      b) Account number
+      c) Type of Account
+      d) Balance amount in the account
+   II. Member Methods(Behaviors):
+      a) To assign initial values
+      b) To deposit an amount
+      c) To withdraw an amount after checking for the balance
+      d) To display name & balance
+  III. Define EXECUTEACCOUNT class that defines main method to test above class.
+   IV. In the above class, maintain the total number of account holders present in the bank and also define a method to define it. Change the main method appropriately.
+    V. In main method of EXECUTEACCOUNT class, define an array to handle five accounts.
+   VI. In ACCOUNT class constructor, demonstrate the use of “this” keyword*/
 
-typedef struct {
-    char name[50];
-    int reg_no;
-    float marks[3];
-    float average_marks;
-} Student;
-
-// Function prototypes
-void read_students(Student *students, int n);
-void display_students(const Student *students, int n);
-void calculate_average(Student *students, int n);
-float calculate_best_two(float marks[]);
-
-int main() {
-    int n, choice;
-    Student *students = NULL;
-
-    printf("Enter the number of students: ");
-    scanf("%d", &n);
-
-    // Dynamically allocate memory for students using calloc
-    students = (Student *)calloc(n, sizeof(Student));
-    if (students == NULL) {
-        printf("Memory allocation failed!\n");
-        return 1;
+// Bank function
+import java.util.*;
+import java.util.Scanner;
+class Bankdetails{
+    String name;
+    String acc_type;
+    String acc_no;
+    int balance;
+    Scanner in = new Scanner(System.in);
+    void openaccount(){
+        System.out.print("Enter name : ");
+        name = in.next();
+        System.out.print("Enter account no. : ");
+        acc_no = in.next();
+        System.out.print("Enter account type : ");
+        acc_type = in.next();
+        System.out.print("Enter balance : ");
+        balance = in.nextInt();
     }
-
-    // For loop to act as a menu-driven program
-    for (;;) {
-        printf("\nMenu:\n");
-        printf("1. Read information of students\n");
-        printf("2. Display students' information\n");
-        printf("3. Calculate average of best two test marks\n");
-        printf("4. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                read_students(students, n);
-                break;
-            case 2:
-                display_students(students, n);
-                break;
-            case 3:
-                calculate_average(students, n);
-                break;
-            case 4:
-                printf("Exiting...\n");
-                free(students);  // Free dynamically allocated memory before exiting
-                return 0;  // Exit the program
-            default:
-                printf("Invalid choice! Please try again.\n");
+    void display(){
+        System.out.println("Name : "+name);
+        System.out.println("Account type : "+acc_type);
+        System.out.println("Account no. : "+acc_no);
+        System.out.println("Balance : "+balance);
+    }
+    void deposit(){
+        System.out.print("Enter amount to deposit : ");
+        int amt = in.nextInt();
+        balance += amt;
+    }
+    void withdraw(){
+        System.out.print("Enter ammount to withdraw : ");
+        int amt = in.nextInt();
+        if(balance >= amt){
+            balance -= amt;
+            System.out.println("Balance after withdrawing "+amt+" = "+balance);
+        }
+        else{
+            System.out.println("Transaction failed!!! Balance is less than amount requested");
         }
     }
-}
-
-// Function to read students' information
-void read_students(Student *students, int n) {
-    for (int i = 0; i < n; i++) {
-        printf("Enter details of student %d:\n", i + 1);
-        printf("Name: ");
-        scanf("%s", students[i].name);
-        printf("Registration Number: ");
-        scanf("%d", &students[i].reg_no);
-        printf("Enter marks for 3 tests:\n");
-        for (int j = 0; j < 3; j++) {
-            printf("Test %d: ", j + 1);
-            scanf("%f", &students[i].marks[j]);
+    public boolean search(String an){
+        if(acc_no.equals(an)){
+            display();
+            return true;
         }
+        return false;
     }
 }
 
-// Function to display students' information
-void display_students(const Student *students, int n) {
-    for (int i = 0; i < n; i++) {
-        printf("\nDetails of student %d:\n", i + 1);
-        printf("Name: %s\n", students[i].name);
-        printf("Registration Number: %d\n", students[i].reg_no);
-        printf("Marks: %.2f, %.2f, %.2f\n", students[i].marks[0], students[i].marks[1], students[i].marks[2]);
-        printf("Average of best two marks: %.2f\n", students[i].average_marks);
-    }
-}
-
-// Function to calculate the average of the best two marks for each student
-void calculate_average(Student *students, int n) {
-    for (int i = 0; i < n; i++) {
-        students[i].average_marks = calculate_best_two(students[i].marks);
-    }
-    printf("Averages calculated successfully!\n");
-}
-
-// Helper function to calculate the average of the best two out of three marks
-float calculate_best_two(float marks[]) {
-    float min = marks[0];
-    float sum = marks[0] + marks[1] + marks[2];
-
-    // Find the minimum mark
-    if (marks[1] < min) min = marks[1];
-    if (marks[2] < min) min = marks[2];
-
-    // Calculate the average of the best two marks
-    return (sum - min) / 2;
+public class Main{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter no. of customers : ");
+        int n = in.nextInt();
+        Bankdetails[] c = new Bankdetails[n];
+        for(int i=0; i<c.length; i++){
+            c[i] = new Bankdetails();
+            c[i].openaccount();
+        }
+        int ch;
+        do{
+            System.out.println("Enter your choice : ");
+            System.out.print("1. Display \n2. Search account by account no.\n3. Deposit\n4. Withdraw\n0. Exit\n");
+            ch = in.nextInt();
+            switch(ch){
+                case 1 : for(int i=0;i<c.length;i++){
+                            c[i].display();
+                         }
+                         break;
+                case 2 : System.out.print("Enter account no. to search : ");
+                         String an = in.next();
+                         boolean found = false;
+                         for(int i=0;i<c.length;i++){
+                             found = c[i].search(an);
+                             if(found){
+                                 break;
+                             }
+                         }
+                         if(!found)
+                            System.out.println("Search failed");
+                         break;
+                case 3 : System.out.print("Enter account no. to deposit : ");
+                         an = in.next();
+                         found  = false;
+                         for(int i=0;i<c.length;i++){
+                             found = c[i].search(an);
+                             if(found){
+                                 c[i].deposit();
+                                 break;
+                             }
+                         }
+                         if(!found)
+                            System.out.println("Search failed");
+                         break;
+                case 4 : System.out.print("Enter accoutn no. to withdraw :  ");
+                         an = in.next();
+                         found = false;
+                         for(int i=0;i<c.length;i++){
+                             found = c[i].search(an);
+                             if(found){
+                                 c[i].withdraw();
+                             }
+                         }
+                         if(!found)
+                            System.out.println("Search failed");
+                         break;
+                case 0 : System.out.println("--------Thankyou-----");
+                         break;
+                default : System.out.println("Invalid choice");
+                          break;
+            }
+        }while(ch != 0):
+ }
 }
