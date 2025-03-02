29adf929c3167e160f8c8d8b89eb2fb32f326ac6
RAKSHA B R <rakshagowdah264@gmail.com>
2025-03-03 00:36:23 +0530

    add

---
+++ Complexnum.java
@@ -0,0 +1,33 @@
+
+class Complexadd {
+  private double real;
+  private double imaginary;
+  static int count;
+  public Complexadd(double real, double imaginary) {
+      this.real = real;
+      this.imaginary = imaginary;
+      count++;
+  }  
+Complexadd add(Complexadd other){
+  double newReal = this.real + other.real;
+  double newImaginary = this.imaginary + other.imaginary;
+  return new Complexadd(newReal, newImaginary);
+}
+void display(){
+  System.out.println(real + " + " + imaginary + "i");
+}
+void getcount(){
+  System.out.println(count);
+}
+
+}
+class Complexnum{
+public static void main(String[] args){
+  Complexadd c1 = new Complexadd(3.2, 4.5);
+  Complexadd c2 = new Complexadd(1.8, 2.3);
+  Complexadd result = c1.add(c2);
+  System.out.print("Sum of complex numbers: ");
+  result.display();
+  result.getcount();
+}
+}
