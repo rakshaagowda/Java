d07b58d08a7f1c21f0b700902272535731cea994
RAKSHA B R <rakshagowdah264@gmail.com>
2025-03-03 00:37:59 +0530

    add

---
+++ Increasesalaray.java
@@ -0,0 +1,28 @@
+class Employee{
+  double salary=50000;
+  void increment(String type){
+    if(type.equals("Fulltime")){
+      salary+=salary*.5;
+    }
+    if(type.equals("Intern")){
+      salary+=salary*.25;
+    
+    }
+      
+    }
+  }
+
+
+
+
+class Increasesalaray{
+  public static void main(String args[]){
+    Employee e=new Employee();
+    e.increment("Fulltime");
+    System.out.println(e.salary); 
+      Employee e1=new Employee(); 
+    e1.increment("Intern");    
+    System.out.println(e1.salary);
+  }
+
+}
