4607352f0d28eb856760c6dcca975b78669b3370
RAKSHA B R <rakshagowdah264@gmail.com>
2025-03-05 19:27:28 +0530

    add

---
+++ Overloading.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+class calculate{
+  void area(int r){
+    System.out.println(3.14*r*r);
+  }
+
+  void area(int l,int b){
+    System.out.println(l*b);
+  }
+}
+class Overloading {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int a = in.nextInt();
+    int b = in.nextInt();
+    int l = in.nextInt();
+    calculate c = new calculate();
+    c.area(a);
+    c.area(l,b);
+    in.close();
+  }  
+}
+
