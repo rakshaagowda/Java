738a5015daa55dd2c0645cb2c37189e56a0e7b88
RAKSHA B R <rakshagowdah264@gmail.com>
2025-03-06 14:07:40 +0530

    add

---
+++ Overriding.java
@@ -0,0 +1,23 @@
+
+class Parent{
+  void method1(){
+    System.out.println("Parent class method");
+  }
+}
+
+class child extends Parent
+  {
+    void method1(){
+  
+      System.out.println("child class method");}
+}
+public class Overriding {
+  public static void main(String[] args) {
+    Parent p= new Parent();
+    p.method1();
+    child c= new child();
+    c.method1();
+
+
+  }
+}
